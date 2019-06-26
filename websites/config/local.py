f2b991894d52b01e8721b9ced492513fc4e47052
huyhoang1996ha <huyhoang1996ha@gmail.com>
2019-06-26 15:20:36 +0700

    Config send mail

--- websites/config/local.py
+++ websites/config/local.py
@@ -13,3 +13,22 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+DEFAULT_TO_ADMIN_EMAIL = "contact@metiz.vn"
+DEFAULT_FROM_EMAIL = "VocSach Service <no-reply@vocsach.vn>"
+# EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+# https://app.sendgrid.com/
+'''
+    Step 1: add your domain to sender authentication
+    Step 2: in your domain, add subdomain from step 1
+    Step 3: Set default for subdomain created
+
+'''
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+
