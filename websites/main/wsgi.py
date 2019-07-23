39e7bb299eabfb3db9f99ace7fe68d69ca9f3088
huyhoang1996ha <huyhoang1996ha@gmail.com>
2019-07-23 16:23:19 +0700

    Config wsgi with setting

--- websites/main/wsgi.py
+++ websites/main/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.production' # or local
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.production") # or local
 
 application = get_wsgi_application()
