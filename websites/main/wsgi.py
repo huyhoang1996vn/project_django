c797b782270e72d31306189cbf5e13fee8921ce6
huyhoang1996ha <huyhoang1996ha@gmail.com>
2019-07-23 16:05:45 +0700

    Config wsgi

--- websites/main/wsgi.py
+++ websites/main/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 application = get_wsgi_application()
